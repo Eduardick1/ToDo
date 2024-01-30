adfc07ff250d24836e17c91c04e3c681967d1b45
Eduard <ed.sushh@gmail.com>
2024-01-30 17:43:20 +0300

    redeploy

--- src/Utils/CssTheme.ts
+++ src/Utils/CssTheme.ts
@@ -4,10 +4,9 @@ const theme = {
   inActiveColor: "#E9E8E8",
   positiveColor: "#65B741",
   negativeColor: "#D71313",
-  fontColor: "#141E46",
   activeFontW: 700,
-  todoPadding: 0.5 + "rem",
-  gap: 2 + "rem",
+  todoPadding: "0.5rem",
+  gap: "2rem",
 };
 
 export default theme;
